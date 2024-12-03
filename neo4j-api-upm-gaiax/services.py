27271a4d69da6374fda46eee24169b9cf79ff6dc
Gorka Zarate <gorka.zarate@tecnalia.com>
2024-12-03 10:02:39 +0100

    add some changes

--- neo4j-api-upm-gaiax/services.py
+++ neo4j-api-upm-gaiax/services.py
@@ -1,3 +1,16 @@
 
 # Función principal para crear nodos y relaciones basado en el JSON
 def create_nodes_and_relationships(session, data):
+    for vc in data["verifiableCredential"]:
+        if "credentialSubject" in vc:
+            subject = vc["credentialSubject"]
+            if subject["type"] == "gx:LegalParticipant":
+                participant_id = subject["id"]
+                legalAddress = subject.get("gx:legalAddress")["gx:countrySubdivisionCode"]
+                create_participant(session, participant_id, subject.get("gx:legalName", "Unknown"), legalAddress)
+
+def create_participant(session, participant_id, legal_name="Unknown", legal_address="Unknown"):
+    session.run(
+        "MERGE (p:Participant {participant_id: $participant_id, name: $name, address: $address})",
+        participant_id=participant_id, name=legal_name, address=legal_address
+    )
